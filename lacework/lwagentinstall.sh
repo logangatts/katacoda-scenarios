462204290d2edf4179954b0efe78b76e8ce4203c
logangatts <logangatts@gmail.com>
2020-12-21 17:35:09 -0600

    added echo

--- lacework/lwagentinstall.sh
+++ lacework/lwagentinstall.sh
@@ -1,3 +1,4 @@
 mkdir /opt/lacework_agent/
 wget https://github.com/lacework/lacework-agent-releases/releases/download/v3.3.5/3.3.5.tgz -P /opt/lacework_agent/
-tar -xvzf /opt/lacework_agent/3.3.5.tgz
+tar -xvzf /opt/lacework_agent/3.3.5.tgz -C /opt/lacework_agent/3.3.5/
+echo "agent install done"
