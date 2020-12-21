6cf2886a6cb6094a061ac299a8c1fd0c827c942e
logangatts <logangatts@gmail.com>
2020-12-21 17:19:22 -0600

    lw script

---
+++ lacework/lwagentinstall.sh
@@ -0,0 +1,3 @@
+mkdir /opt/lacework_agent/
+wget https://github.com/lacework/lacework-agent-releases/releases/download/v3.3.5/3.3.5.tgz -P /opt/lacework_agent/
+tar -xvzf /opt/lacework_agent/3.3.5.tgz
